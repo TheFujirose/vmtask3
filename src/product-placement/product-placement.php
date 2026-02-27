e39eefb2f73ba839c70f11adc8efee96c2eaa357
TheFujirose <50027005+TheFujirose@users.noreply.github.com>
2026-02-26 21:00:35 -0500

    Feat: Plugin init

---
+++ src/product-placement/product-placement.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     product-placement
+ * Plugin URI:      https://github.com/TheFujirose/vmtask3
+ * Description:     Woocommerce Elementor widget
+ * Author:          Carson Fujita
+ * Author URI:      https://thefujirose.github.io/
+ * Text Domain:     product-placement
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Product_Placement
+ */
+
+// Your code starts here.
