68469c02ba66da2827e6d32057ce168c6ffebf0d
TheFujirose <50027005+TheFujirose@users.noreply.github.com>
2026-02-26 20:09:10 -0500

    Feat: Roots frameworks

---
+++ src/trunk/vmtask3.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * VMTASK3
+ *
+ * @package vmtask3
+ * @author Carson Fujita
+ * @copyright 2026 Carson Fujita
+ * @license MIT
+ * @version 1.0.0
+ *
+ * @wordpress-plugin
+ * Plugin Name: VMTASK3
+ * Plugin URI: https://github.com/TheFujirose/vmtask3
+ * Description: Implementation of WooCommerce functionalities within a website builder section of Elementor.
+ * Version: 1.0.0
+ * Requires at least: 6.9
+ * Requires PHP: 8.2
+ * Author: Carson Fujita
+ * License: MIT
+ * License URI: https://github.com/TheFujirose/vmtask3/blob/main/README.md
+ * Requires Plugins: WooCommerce Elementor
+ */
